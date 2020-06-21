b55d67ef9ac5385d09ecec0dd13d65112566c3ca
mohamed <mohamedsaid.2001@hotmail.com>
2020-06-21 17:20:43 +0200

    ik heb de kleuren veranderd

--- java.js
+++ java.js
@@ -20,16 +20,83 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("slide2");
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    slides[slideIndex-1].style.display = "block";
+}
+
+
+
+var slideIndex3 = 1;
+showSlides3(slideIndex);
+
+function plusSlides3(n) {
+    showSlides3(slideIndex += n);
+}
+
+function currentSlide3(n) {
+    showSlides3(slideIndex = n);
+}
+
+function showSlides3(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides3");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+
 }
+
+
+var slideIndex4 = 1;
+showSlides4(slideIndex);
+
+function plusSlides4(n) {
+    showSlides4(slideIndex += n);
+}
+
+function currentSlide4(n) {
+    showSlides4(slideIndex = n);
+}
+
+function showSlides4(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides4");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+
+}
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+/*Scroll to top when arrow up clicked END*/
+
